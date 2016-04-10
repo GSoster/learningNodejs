eb6605d821c309fddf730247daef456844af1436
GSoster <guilhermesosters@gmail.com>
2016-04-10 17:42:27 -0300

    starting to use the connect module

--- thenewboston_node_tutorial/server.js
+++ thenewboston_node_tutorial/server.js
@@ -1,15 +1,28 @@
-var http = require('http');
-//the function that will be passed as parameter to the http.createServer function receives two parameters: request, response. 
-function onRequest(request, response) {
-    console.log("A user made a request: ", request.url);
-    response.writeHead(200, {
-        "Content-Type": "text/plain"
-    }); //status code, reason, obj
-    response.write("Here is some data");
-    response.end();
-};
+var connect = require("connect");
+var http = require("http");
 
-//we pass on createServer what the server should do.
-//we pass the port that the server will listen to in the listen function.
-http.createServer(onRequest).listen(8888);
-console.log("Server is now running....");
+var app = connect();
+
+function doFirst(request, response, next) {
+    console.log("a request was made: first");
+    next(); //call the next method in the stack
+}
+
+function doSecond(request, response, next) {
+    console.log("a request was made: second");
+    next();
+}
+//create a stack of methods to be executed
+app.use(doFirst);
+app.use(doSecond);
+
+//we can use the connect module as a router too
+function profile(request, response) {
+    console.log('user has required profile');
+}
+app.use('/profile', profile);
+
+
+//server start
+http.createServer(app).listen(8888);
+console.log('server is running...');
