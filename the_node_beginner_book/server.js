103f35c72e1101372f077c6930a15e18f3722f77
GSoster <guilhermesosters@gmail.com>
2016-05-09 17:11:58 -0300

    Added project from the book: The Node Beginner Book

---
+++ the_node_beginner_book/server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var url = require('url'); //our router needs it to get the path
+
+
+function start(route, handle) {
+
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        route(handle, pathname);
+
+        response.writeHead(200, {
+            "Content-Type": "text/plain"
+        });
+        response.write("it started");
+        response.end();
+    }
+
+
+
+    http.createServer(onRequest).listen(80);
+    console.log("Server has started");
+
+}
+
+exports.start = start;
