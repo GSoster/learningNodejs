d45d73c715ec241775f8ae533ad75b0b5a3fec2a
Guilherme Soster <guilhermesosters@gmail.com>
2016-05-26 20:14:18 -0300

    finished the book examples, source code complete

--- the_node_beginner_book/server.js
+++ the_node_beginner_book/server.js
@@ -5,27 +5,9 @@ route here is a function from ./router.js
 */
 function start(route, handle) {
     function onRequest(request, response) {
-        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
-
-        request.setEncoding("utf8");
-
-        /*
-        *requests work in two ways on nodejs: 'data' (chucks of data) and 'end' when all the request is received
-        */
-        request.addListener("data", function(postDataChuck){
-          //called when a new chuck of data was received
-          postData += postDataChuck;
-          console.log("received chuck of data: " + postDataChuck);
-        });
-
-        request.addListener("end", function(){
-          //called when all chucks of data have been received
-            route(handle, pathname, response, postData);
-            //we took response from here and now it is route's responsability to emit a response.
-        });
-
+        route(handle, pathname, response, request);
     };
 
     http.createServer(onRequest).listen(80);
