493cf804fa1ab1fd2d47ae716b4d054f9fcadbc4
Guilherme Soster <guilhermesosters@gmail.com>
2017-03-04 14:54:11 -0300

    starting to work on server.js file

---
+++ restAPI_express/app/server.js
@@ -0,0 +1,28 @@
+//server.js
+
+//BASE SETUP
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+//ROUTES FOR OUR API
+var router = express.Router();
+
+router.get('/', function(req, res){
+  res.json({message: 'hello world!'});
+});
+
+app.use('/api', router);
+
+//START THE server
+app.listen(port);
+console.log('server started at: ' + port);
